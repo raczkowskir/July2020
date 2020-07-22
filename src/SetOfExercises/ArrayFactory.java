f70a590cecf572d7a49744d12e1b7cb540506352
raczkowskir <raczkowskir@yahoo.com>
2020-07-22 21:19:17 +0200

    Simple Exercise 5

--- src/SetOfExercises/ArrayFactory.java
+++ src/SetOfExercises/ArrayFactory.java
@@ -11,7 +11,31 @@ public class ArrayFactory {
         ArrayFactory af = new ArrayFactory(4);
 
         int[] myNewArray = af.oneDimention();
-        int [][] myNewArray2 = af.twoDimentions();
+        int[][] myNewArray2 = af.twoDimentions();
+
+
+        af.displayNumberFromBackWords(1234);
+        af.displayStringFromBackWords("No problems.");
+
+    }
+
+    private void displayNumberFromBackWords(int number) {
+        String numberInString = String.valueOf(number);
+        int numberInStringLength = numberInString.length();
+
+        for (int i = 0; i < numberInStringLength; i++) {
+            System.out.println(numberInString.substring(( numberInStringLength - (i + 1)), numberInStringLength - i));
+
+        }
+    }
+
+    private void displayStringFromBackWords(String word) {
+        int wordLength = word.length();
+
+        for (int i = 0; i < wordLength; i++) {
+            System.out.println(word.substring(( wordLength - (i + 1)), wordLength - i));
+
+        }
     }
 
     private int[] oneDimention() {
